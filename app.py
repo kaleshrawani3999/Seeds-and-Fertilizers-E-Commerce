c85a31993f33000707020f67ca0580d4953c1e55
Shrawani Kale <kaleshrawani3999@gmail.com>
2026-04-07 00:30:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,7 @@ db = mysql.connector.connect(
     port=int(os.environ.get("3306", 3306))
 )
 conn = get_db_connection()
-cursor = conn.cursor()
-# ... do your queries
-cursor.close()
-conn.close()
+
 # ---------------- REGISTER ----------------
 @app.route('/register', methods=['GET','POST'])
 def register():
